3cb8ee6722a5b737b3759a4a833136f8b556b748
mattbarna <barna10@msn.com>
2026-03-31 13:02:28 -0400

    Adding AI

--- web/src/main/java/com/eliassen/crucible/web/drivers/mocks/MockTimeouts.java
+++ web/src/main/java/com/eliassen/crucible/web/drivers/mocks/MockTimeouts.java
@@ -7,13 +7,8 @@ import java.util.concurrent.TimeUnit;
 
 public class MockTimeouts implements WebDriver.Timeouts {
     @Override
-    public WebDriver.Timeouts implicitlyWait(long time, TimeUnit unit) {
-        return null;
-    }
-
-    @Override
     public WebDriver.Timeouts implicitlyWait(Duration duration) {
-        return WebDriver.Timeouts.super.implicitlyWait(duration);
+        return null;
     }
 
     @Override
@@ -22,18 +17,8 @@ public class MockTimeouts implements WebDriver.Timeouts {
     }
 
     @Override
-    public WebDriver.Timeouts setScriptTimeout(long time, TimeUnit unit) {
-        return null;
-    }
-
-    @Override
-    public WebDriver.Timeouts setScriptTimeout(Duration duration) {
-        return WebDriver.Timeouts.super.setScriptTimeout(duration);
-    }
-
-    @Override
     public WebDriver.Timeouts scriptTimeout(Duration duration) {
-        return WebDriver.Timeouts.super.scriptTimeout(duration);
+        return null;
     }
 
     @Override
@@ -42,13 +27,8 @@ public class MockTimeouts implements WebDriver.Timeouts {
     }
 
     @Override
-    public WebDriver.Timeouts pageLoadTimeout(long time, TimeUnit unit) {
-        return null;
-    }
-
-    @Override
     public WebDriver.Timeouts pageLoadTimeout(Duration duration) {
-        return WebDriver.Timeouts.super.pageLoadTimeout(duration);
+        return null;
     }
 
     @Override
